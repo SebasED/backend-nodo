85fdf47bb0e2866b4e6b02e628ca625b3dcb27e2
SebasED <sebas23505@gmail.com>
2024-10-23 16:23:24 -0500

    Feat: SpringBoot project created

---
+++ estramipyme/src/main/java/estramipyme/EstramipymeApplication.java
@@ -0,0 +1,13 @@
+package estramipyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstramipymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstramipymeApplication.class, args);
+	}
+
+}
---
+++ estramipyme/src/test/java/estramipyme/EstramipymeApplicationTests.java
@@ -0,0 +1,13 @@
+package estramipyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstramipymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
