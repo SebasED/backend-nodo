6456d102cbe2f6f9ed3eb6dbe47ccea7da84704b
John Sebastian Estrada Durango <sebas23505@gmail.com>
2024-11-04 20:01:01 -0500

    build, refactor: dotenv-java and spring-boot-devtools libreries added. Added environment variables and url form

--- estramipyme/src/main/java/estramipyme/controller/QuestionController.java
+++ estramipyme/src/main/java/estramipyme/controller/QuestionController.java
@@ -3,23 +3,22 @@ package estramipyme.controller;
 import estramipyme.model.Question;
 import estramipyme.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/question")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET})
 public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/all")
-    public List<Question> getAllQuestions() {
-        return questionService.getAllQuestions();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+
+        return ResponseEntity.ok(questionService.getAllQuestions());
     }
 }
